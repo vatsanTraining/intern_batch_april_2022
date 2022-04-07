ab58fbe128cdef0947e535c86717ec81eb760e53
srivatsan <vatsan_k@hotmail.com>
2022-04-07 11:16:17 +0530

    First Commit

---
+++ quickstart/src/com/training/Application.java
@@ -0,0 +1,25 @@
+package com.training;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		Greeting grtObj = new Greeting();
+		
+		// System s = new System();
+		
+		
+		System.out.println(grtObj.getMessage());
+		
+		System.out.println(Greeting.getInfo());
+		
+		System.out.println(grtObj.getMessage());
+		
+		// System.out.println(grtObj.getInfo());
+		
+		int a =012;
+		
+		System.out.println("Value of A:="+a);
+		
+	}
+}
---
+++ quickstart/src/com/training/Greeting.java
@@ -0,0 +1,17 @@
+package com.training;
+
+ public class Greeting {
+	
+	
+	public String getMessage() {
+		
+		return "Welcome to Java Programming";
+	}
+
+	public static String getInfo() {
+		
+		return "Hello From static Method";
+	}
+	
+	
+}
